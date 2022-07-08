38ebac5abcb0db72e967a6b2d403aef3be0a4aec
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2022-07-08 15:06:44 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -17,25 +17,25 @@ use App\Http\Controllers\Api\QueueController;
 */
 
 //API route for register new user
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->middleware(['cors']);
 //API route for login user
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->middleware(['cors']);
 
 //Protecting Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function(Request $request) {
         return auth()->user();
-    });
+    })->middleware(['cors']);
 
     // Queue
     Route::prefix('queue')->group(function () {
-        Route::get('/', [QueueController::class, 'index']);
-        Route::get('/show/{id}', [QueueController::class, 'show']);
-        Route::post('/store', [QueueController::class, 'store']);
-        Route::post('/update/{id}', [QueueController::class, 'update']);
-        Route::delete('/delete/{id}', [QueueController::class, 'delete']);
+        Route::get('/', [QueueController::class, 'index'])->middleware(['cors']);
+        Route::get('/show/{id}', [QueueController::class, 'show'])->middleware(['cors']);
+        Route::post('/store', [QueueController::class, 'store'])->middleware(['cors']);
+        Route::post('/update/{id}', [QueueController::class, 'update'])->middleware(['cors']);
+        Route::delete('/delete/{id}', [QueueController::class, 'delete'])->middleware(['cors']);
     });
 
     // API route for logout user
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware(['cors']);
 });
