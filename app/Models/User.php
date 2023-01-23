31e558ebd99ad59828c4a9b698d13a90a247929a
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2023-01-23 07:44:16 +0700

    update project

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'address',
+        'date_of_birth',
+        'age',
+        'gender',
         'password',
     ];
 
